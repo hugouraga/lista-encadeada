22235278fd85409e159903b06ab92f890833e5a0
Hugo Issao Uraga <hugouraga61@gmail.com>
2018-05-24 20:29:27 -0300

    Update lista encadeada.py

--- lista encadeada.py
+++ lista encadeada.py
@@ -2,18 +2,22 @@ class No:
     def __init__(self,item = None,proximo = None):
         self.item = item
         self.proximo = proximo
+        
 class Lista:
     def __init__(self):
         self.primeiro = self.ultimo = No()
+        self.tamanhoLista = 0
     def vazia(self):
         return self.primeiro == self.ultimo
     def inserir(self,item):
         self.ultimo.proximo = No(item)
         self.ultimo = self.ultimo.proximo
+        self.tamanhoLista += 1
     def inserirInicio(self, item):
         self.primeiro.proximo = No(item,self.primeiro.proximo)
         if self.vazia():
             self.ultimo = self.primeiro.proximo
+        self.tamanhoLista += 1 
     def inserirOrdenado(self,item):
         if vazia():
             self.inserir(item)
@@ -25,9 +29,10 @@ class Lista:
         anterior.proximo = No(item,atual)
         if atual is None:
             self.ultimo = anteiror.proximo
+        self.tamanhoLista += 1
     def pesquisa(self, item):
         aux = self.primeiro.proximo
-        while not aux in None and aux.item != item:
+        while not aux is None and aux.item != item:
             aux = aux.proximo
         return aux is None and None or aux.item
     def removerInicio(self):
@@ -53,3 +58,28 @@ class Lista:
         ultimo.proximo = None
         del aux
         return item
+    def __str__(self):
+        s = "["
+        aux = self.primeiro.proximo
+        while not aux is None:
+            s += str(aux.item) + ','
+            aux = aux.proximo
+        s = s.strip(",")
+        s += "]"
+        return s
+    def __getitem__(self, index):
+        if self.vazia():
+            return IndexError("A lista se encontra vazia")
+        if index > tamanhoLista or index < 0:
+            return IndexError("index inválido")
+        aux = self.primeiro.proximo
+        ponteiro = 0
+        while index > ponteiro:
+            aux = aux.proximo
+            ponteiro += 1
+        if ponteiro == index:
+            return aux.valor
+    def __len__(self):
+        return self.tamanhoLista
+        
+        
